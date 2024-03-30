d01310fb039a032877000aff672f2bf89b4405f8
Priya Gupta <pg7108970@gmail.com>
2024-03-30 21:54:29 +0530

    commit all

--- java/src/nested.java
+++ java/src/nested.java
@@ -1,7 +1,15 @@
-public class nested loop {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class nested {
+    public static void main(String[] args) {
+        System.out.println("Enter input:");
+        double num = Scanner.nextDouble();
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                System.out.println(" * ");
+            }
+            System.out.println();
+        }
     }
 
-    }
-
+}
