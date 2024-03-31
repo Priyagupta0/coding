c4b301abc22f4ed49c05fef62b60e22325338257
Priya Gupta <pg7108970@gmail.com>
2024-04-01 00:39:53 +0530

    commit all

--- java/src/nestedreverse.java
+++ java/src/nestedreverse.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class nestedreverse {
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner S = new Scanner(System.in);
         System.out.println("enter input:");
         int n = S.nextInt();
