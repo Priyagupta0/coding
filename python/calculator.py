1f3ea137ac9932504fec540115821266cde9847c
Priya Gupta <pg7108970@gmail.com>
2024-04-07 16:43:31 +0530

    commit all

--- python/calculator.py
+++ python/calculator.py
@@ -2,7 +2,7 @@ i=0
 while i<10:
 
     print("\t""\t""OUR CALCULATOR")
-    print(" 1.) ADDITION""\n"" 2.) SUBTRACTION""\n"" 3.) DIVISION""\n"" 4.) MULTIPLICATION""\n")
+    print(" 1.) ADDITION""\n"" 2.) SUBTRACTION""\n"" 3.) DIVISION""\n"" 4.) MULTIPLICATION""\n"" 5.) Print Table""\n")
     A = int(input("choose one:"))
     if A==1:
         a=float(input("Enter value :"))
@@ -27,6 +27,12 @@ while i<10:
         b=float(input("Enter value :"))
         c=a*b
         print(c)
+
+    elif A==5:
+        num =int(input("enter number for table :"))
+        for i in range(1,11):
+            print (str(num) + " X " + str(i) + " = " + str(i*num))
+    
 
     else:
         print("not in a list ")
