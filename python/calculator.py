7e15ec37720ff6a8dd1a6bd47b7ea6add0e39cb1
Priya Gupta <pg7108970@gmail.com>
2024-04-07 16:18:16 +0530

    commit all

--- python/calculator.py
+++ python/calculator.py
@@ -1,29 +1,34 @@
-print("\t""\t""OUR CALCULATOR")
-print(" 1.) ADDITION""\n"" 2.) SUBTRACTION""\n"" 3.) DIVISION""\n"" 4.) MULTIPLICATION""\n")
-A = int(input("choose one:"))
-if A==1:
-    a=float(input("Enter value :"))
-    b=float(input("Enter value :"))
-    c=a+b
-    print(c)
+i=0
+while i<10:
 
-elif A==2:
-    a=float(input("Enter value :"))
-    b=float(input("Enter value :"))
-    c=a-b
-    print(c)
+    print("\t""\t""OUR CALCULATOR")
+    print(" 1.) ADDITION""\n"" 2.) SUBTRACTION""\n"" 3.) DIVISION""\n"" 4.) MULTIPLICATION""\n")
+    A = int(input("choose one:"))
+    if A==1:
+        a=float(input("Enter value :"))
+        b=float(input("Enter value :"))
+        c=a+b
+        print(c)
 
-elif A==3:
-    a=float(input("Enter value :"))
-    b=float(input("Enter value :"))
-    c=a/b
-    print(c)
+    elif A==2:
+        a=float(input("Enter value :"))
+        b=float(input("Enter value :"))
+        c=a-b
+        print(c)
 
-elif A==4:
-    a=float(input("Enter value :"))
-    b=float(input("Enter value :"))
-    c=a*b
-    print(c)
+    elif A==3:
+        a=float(input("Enter value :"))
+        b=float(input("Enter value :"))
+        c=a/b
+        print(c)
 
-else:
-    print("not in a list ")
+    elif A==4:
+        a=float(input("Enter value :"))
+        b=float(input("Enter value :"))
+        c=a*b
+        print(c)
+
+    else:
+        print("not in a list ")
+
+    
