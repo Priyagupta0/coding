0185b055eab995a51a2c7193995f7865616f2a86
Priya Gupta <pg7108970@gmail.com>
2024-04-09 21:45:42 +0530

    commit all

---
+++ python/forloop_pattern.py
@@ -0,0 +1,35 @@
+n=3
+for i in range(3):
+    print(" "*(n-i-1),end="")
+    print("*" * (2*i+1),end="")
+    print(" "*(n-i-1))
+print("\n")
+for i in range(3):
+    print("*"*(2*i+1))
+print("\n")
+for i in range(3):
+    print("  "*(n-i-1),end="")
+    print("*"*(2*i+1))
+print("\n")
+for i in range(3):
+    print("*"*(i+1))
+print("\n")
+for i in range(4):
+    print("*"*(2*i))
+print("\n")
+for i in range(3):
+    print("*"*(n))
+    n=n-1
+print("\n")
+print("*****")
+print(" *** ")
+print("  *  ")
+print("\n")
+print("***")
+print("* *")
+print("***")
+print("\n")
+print("****")
+print(" ** ")
+print("****")
+print("\n")
