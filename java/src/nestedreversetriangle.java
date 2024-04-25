c7e9337de7ffee282d5fe5d181cf69c48b9c5533
Priya Gupta <pg7108970@gmail.com>
2024-04-25 22:04:33 +0530

    commit all

--- java/src/nestedreversetriangle.java
+++ java/src/nestedreversetriangle.java
@@ -12,6 +12,12 @@ public class nestedreversetriangle {
             }
             System.out.println();
         }
+        // for(int i=4;i>=1;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
     }
 
 }
