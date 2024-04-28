bea21c3b9699f62f93afbd82e978c2c79369d212
Priya Gupta <pg7108970@gmail.com>
2024-04-28 12:26:10 +0530

    commit all

--- java/src/nestednumber.java
+++ java/src/nestednumber.java
@@ -45,5 +45,50 @@ public class nestednumber {
             }
             System.out.println();
         }
-    }
+        System.out.print("\n");
+
+        //number pyramid 
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int j=1;j>i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.print("\n");
+
+        //number pyrmid
+
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+        System.out.print("\n");
+
+        //number pyramid
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            for(int j=2;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+    }                         
 }
