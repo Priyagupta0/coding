6fd93c56ec63b56a93857567fa925d1a2e8d69f5
Priya Gupta <pg7108970@gmail.com>
2024-04-28 12:54:37 +0530

    commit all

--- java/src/diamondnested.java
+++ java/src/diamondnested.java
@@ -1,3 +1,30 @@
 public class diamondnested {
-    
-}
+    public static void main(String args[]){
+        for(int i=1;i<=4;i++){
+            for(int j=0;j<4-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<2*i-1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<4-i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+            for(int i=1;i<=3;i++){
+                for(int j=0;j<i;j++){
+                    System.out.print(" ");
+                }
+                for(int j=0;j<5-2*i+2;j++){
+                    System.out.print("*");
+                }
+                for(int j=0;j<i;j++){
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
