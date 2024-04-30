6477fabd8d1a58a89447a58754dce5d43c51595f
Priya Gupta <pg7108970@gmail.com>
2024-04-30 12:51:14 +0530

    commit all

---
+++ java/src/factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class factorial {
+    public static int CalculateFactorial(int x) {
+        int fact=1;
+        while(x>1){
+        fact=fact*x;
+        x=x-1;
+        }
+        return fact ;
+    }
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int num = s.nextInt();
+        int fact =CalculateFactorial(num);
+        System.out.print("factorial of "+num+" is : "+fact);
+    }
+}
