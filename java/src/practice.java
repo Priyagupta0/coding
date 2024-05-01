5eb08e1f705d783c9aaebf35f987c80464406243
Priya Gupta <pg7108970@gmail.com>
2024-05-01 08:20:44 +0530

    commit all

---
+++ java/src/practice.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class A{
+    public void setValue(int x){
+        int value = x;
+    }
+    public int getValue(int value){
+        return ++value;
+    }
+}
+public class practice {
+    public static void main(String args[]){
+        A r = new A();
+        int result = r.getValue(100);
+        System.out.print(result);
+    }
+}
