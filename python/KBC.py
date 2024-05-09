272c67c081892c1cd4fbc8dd976f722d4f1a5083
Priya Gupta <pg7108970@gmail.com>
2024-05-09 21:14:56 +0530

    commit all

--- python/KBC.py
+++ python/KBC.py
@@ -1,27 +1,39 @@
 import random
+
 #Question list
 def question(comp,user):
     if(comp==0 and user==3):
+        print("Correct Answer is 3")
         return 1
     elif(comp==2 and user==4):
+        print("Correct Answer is 4")
         return 1
     elif(comp==1 and user==3):
+        print("Correct Answer is 3")
         return 1
     elif(comp==3 and user==2):
+        print("Correct Answer is 2")
         return 1
     elif(comp==4 and user==4):
+        print("Correct Answer is 4")
         return 1
     elif(comp==5 and user==2):
+        print("Correct Answer is 2")
         return 1
     elif(comp==6 and user==1):
+        print("Correct Answer is 1")
         return 1
     elif(comp==7 and user==4):
+        print("Correct Answer is 4")
         return 1
     elif(comp==8 and user==2):
+        print("Correct Answer is 2")
         return 1
     else:
         return -1
-print("\t\tAre you Ready for KBC....")
+print("\t\tWelcome to Kaun Banega Crorepati....")
+print("\tYou have only 10 second to answer the question.")
+print("\t\t\tLet's Begin!!!")
 comp=random.randint(0,8)
 if(comp==0):
     print("Which city is known as Pink City in India?\n 1. Banglore \n 2.Maysore \n 3.Jaipur \n 4.Kochi")
@@ -30,7 +42,7 @@ if(comp==1):
 if(comp==2):
     print("Which god is also known as ‘Gauri Nandan’? \n 1.Agni \n 2.Indra \n 3.Hanuman \n 4.Ganesha")
 if(comp==3):
-    print("When is the National Hindi Diwas celebrated? \n1.13 September \n 2.14 September\n3.14 July\n4.15 August")
+    print("When is the National Hindi Diwas celebrated? \n1.13 September \n2.14 September\n3.14 July\n4.15 August")
 if(comp==4):
     print("Which one of the following places is famous for the Great Vishnu Temple?\n1.Bordubar, Indonesia\n2.Bamiyan, Afghanistan\n3.Panja Sahib, Pakistan\n4.Ankorvat, Cambodia")
 if(comp==5):
@@ -42,9 +54,8 @@ if(comp==7):
 if(comp==8):
     print("How do you say Hello in French?\n1.Neeche\n2.Bonjour\n3.Namaste\n4.Chor")
 user=int(input("write your answer please in Question Sr.No.:"))
-print("You:",user)
 score=question(comp,user)
-if score==1:
+if score== -1:
+     print("You Lose!..Try your Luck next time ")  
+elif score== 1:
     print("You Won 1 Crore!!!")
-elif score== -1:
-    print("You Lose!..Try your Luck next time ")
