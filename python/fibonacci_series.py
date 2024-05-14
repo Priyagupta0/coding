c75742ca5eefabb96a568cd422e26015bceb02c4
Priya Gupta <pg7108970@gmail.com>
2024-05-14 15:03:35 +0530

    commit all

---
+++ python/fibonacci_series.py
@@ -0,0 +1,16 @@
+n=int(input("Enter number for series (n>2):"))
+x1=0
+x2=1
+if(n<0):
+    print("Enter positive number")
+elif(n==1):
+    print("Fibonacci series:",x1)
+elif(n==2):
+    print("Fibonacci series:",x1,x2)
+elif(n>2):
+    print("Fibonacci series:",x1,x2,end=' ')
+    for n in range(n):
+      x=x1+x2
+      x1=x2
+      x2=x
+      print( x,end=" ")
