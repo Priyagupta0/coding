0577369bf6930b14e98ad440ef27323bba9aa2ba
Priya Gupta <pg7108970@gmail.com>
2024-05-16 17:49:04 +0530

    commit all

--- python/calendar.py
+++ python/calendar.py
@@ -1,7 +1,11 @@
 import calendar
-
 year=int(input("Enter year:"))
 month=int(input("Enter month:"))
+print(calendar.month(year,month))
 
-cal=calendar.month(year,month)
-print(cal)
+# print()
+
+# from datetime import datetime
+# now=datetime.now()
+# dt_string=now.strftime("%d/%m/%Y %H:%M:%S")
+# print("date and time = ",dt_string)
