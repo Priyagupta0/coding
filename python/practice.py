ca13f0aef0c711761d7f74032a0cbbe593971b5b
Priya Gupta <pg7108970@gmail.com>
2024-05-18 18:45:43 +0530

    commit all

--- python/practice.py
+++ python/practice.py
@@ -1,18 +1,24 @@
-def sorted_string(string):
-    words=string.split()
-    words.sort()
-    new_sorted=" , ".join(words)
-    return new_sorted
+# def sorted_string(string):               #question 1 
+#     words=string.split()
+#     words.sort()
+#     new_sorted=" , ".join(words)
+#     return new_sorted
 
-input_string=input("Enter:")
-print(sorted_string(input_string))
+# input_string=input("Enter:")
+# print(sorted_string(input_string))
 
 
-def with_commas(string):
-    result=""
-    for character in string:
-        result+= character+","
-    return result[:-1]
+# def with_commas(string):                 #question 2
+#     result=""
+#     for character in string:
+#         result+= character+","
+#     return result[:-1]
 
-input_string=input("Enter:")
-print(with_commas(input_string))
+# input_string=input("Enter:")
+# print(with_commas(input_string))
+
+
+# list_fruits=["Apple","Banana","Mango","Grapes"]
+# for index,fruits in  enumerate(list_fruits,start=1):              #Enumerate function
+#     print(f"{index}. {fruits}")
+
