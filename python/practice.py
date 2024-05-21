5aa5a37aad772bd2674e2bd6019a71a7363c4623
Priya Gupta <pg7108970@gmail.com>
2024-05-21 14:20:09 +0530

    commit all

--- python/practice.py
+++ python/practice.py
@@ -22,3 +22,22 @@
 # for index,fruits in  enumerate(list_fruits,start=1):              #Enumerate function
 #     print(f"{index}. {fruits}")
 
+# sen=input("Enter input :")                        #count words,digits,uppercase,lowercase
+# w,d,u,l=0,0,0,0
+# s=sen.split()
+# w=len(s) 
+# for c in sen:
+#     if c.isdigit():
+#         d=d+1
+#     elif c.isupper():
+#         u=u+1
+#     elif c.islower():
+#         l=l+1
+# print("No.of words :",w)
+# print("No.of Digits:",d)
+# print("No.of Uppercase letters:",u)
+# print("No.of Lowercase letters:",l)
+
+
+
+
