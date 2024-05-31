9296244c7ebf74d4478aead5822900a9a58651e3
Priya Gupta <pg7108970@gmail.com>
2024-05-31 13:51:43 +0530

    commit all

--- python/sketch.py
+++ python/sketch.py
@@ -1,12 +1,15 @@
-# from sketchpy import library as lib
-# obj=lib.flag()
-# obj.draw()
+from sketchpy import library as lib
+obj=lib.flag()
+obj.draw()
 # from sketchpy import library as lib
 # obj=lib.apj()
 # obj.draw()
 # from sketchpy import canvas as lib
 # obj=lib.sketch_from_image("python/msdhoni.webp")
 # obj.draw()
-from sketchpy import canvas as lib
-obj=lib.sketch_from_image("python/anshika.jpg")
-obj.draw()
+# from sketchpy import canvas as lib
+# obj=lib.sketch_from_image("python/anshika.jpg")
+# obj.draw()
+# from sketchpy import canvas as lib
+# obj=lib.sketch_from_image("python/my image.jpg")
+# obj.draw()
